279ee7c2519a6ac9e15dea35ff040e9b77627222
Claus Bové <clbo@kea.dk>
2015-09-02 17:21:13 +0200

    inistial files pushed

---
+++ index.js
@@ -0,0 +1,12 @@
+// you can delete this 
+// before you put your own code in here
+
+var x = {
+	helloString : 'Hello',
+	worldString : 'World',
+	concatenation : function () {
+		return this.helloString + ' ' + this.worldString;
+	}
+}
+
+console.log(x.concatenation());
